5e0d563053979e5f71762da063eff40c0f6e384f
Tarcisio 'Kbça' Cantalice <tarcisiocantalice@gmail.com>
2019-05-09 14:14:46 -0300

    APP: Compilação de aplicação 0.0.1

--- src/main/java/com/nipsters/App.java
+++ src/main/java/com/nipsters/App.java
@@ -1,5 +1,7 @@
 package com.nipsters;
 
+import com.nipsters.view.MainView;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MainView.getInstance().showView();
     }
 }
