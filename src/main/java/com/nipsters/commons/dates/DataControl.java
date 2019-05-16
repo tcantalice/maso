f92e88d9cff622a1a9b1dee1c1a73302e38b1f6a
brendo.pinheiro@ucsal.edu.br <Brendo Souza pinheiro>
2019-05-16 11:47:02 -0300

    Ferramenta para trabalhar com datas v4

--- src/main/java/com/nipsters/commons/dates/DataControl.java
+++ src/main/java/com/nipsters/commons/dates/DataControl.java
@@ -43,6 +43,7 @@ public static LocalDate formatData(String data) throws IllegalArgumentException 
 	}
 	
 	public static int birthdayDate(LocalDate birthDate) {
+		
 		int age = LocalDate.now().getYear() - birthDate.getYear();
 		
 		if(LocalDate.now().getMonthValue() < birthDate.getMonthValue()) {
--- src/test/java/com/nipsters/commons/dates/DataControlTest.java
+++ src/test/java/com/nipsters/commons/dates/DataControlTest.java
@@ -42,6 +42,7 @@ public class DataControlTest {
 	
 	@Test
 	public void testeIdade() {
+		
 		System.out.println(DataControl.birthdayDate(DataControl.formatData("17/07/2000")));
 		System.out.println(DataControl.birthdayDate(DataControl.formatData("16/03/1997")));
 		System.out.println(DataControl.birthdayDate(DataControl.formatData("01/04/2000")));
